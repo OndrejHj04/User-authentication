8ffa3b059fcb74c0ad9ff4a9e15d72120028ad6d
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-03-25 18:21:52 +0100

    User-autentication[3]

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,19 @@
-export default function Home(props){
+import { useState } from "react";
+import database from "./db.json";
+
+export default function Home(props) {
 
 
 
-    return(
+  return (
+    <>
+      {!props.user && <h1>Sign up or sign in to use this app!</h1>}
+      {props.user && (
         <>
-        {!props.user && <h1>Sign up or sign in to use this app!</h1>}
-        {props.user && <h1>Add new friends!</h1>}
+          <h1>Add new friends!</h1>
+          <div className="card-container">{page}</div>
         </>
-    )
+      )}
+    </>
+  );
 }
